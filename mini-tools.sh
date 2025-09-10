4aa106083d7355892e30bf742a5a6ea07c8c02f1
Lily <8723107+lily-gh@users.noreply.github.com>
2025-09-10 11:48:09 +0200

    feat: added mini-tools.sh for setting up aliases for bash

---
+++ mini-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setting up aliases..."
+
+touch ~/.bashrc
+echo "alias l=\"ls -l\"" >> ~/.bashrc
+echo "alias ls=\"ls -l\"" >> ~/.bashrc
+echo "alias la=\"ls -la\"" >> ~/.bashrc
