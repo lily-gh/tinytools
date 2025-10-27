88109a4f2cb02329b042b4a0f158e02f70baf4cb
Lily <8723107+lily-gh@users.noreply.github.com>
2025-10-27 16:12:55 +0100

    feat: add basic vim configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tiny Tools
+Useful small tools, mostly used when connecting to some server that doesn't have my usual settings.
+
+## How to use
+Simply run the following command in order to fetch and execute `mini-tools.sh` when connected to any computer:
+
+```bash
+curl -FsSL tools.lilyoliveira.com | bash
+```
--- mini-tools.sh
+++ mini-tools.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-echo "Setting up aliases..."
+echo "* Setting up Lily's Tiny Tools *"
 
+echo "Setting up aliases..."
 touch ~/.bashrc
 echo "alias l=\"ls -l\"" >> ~/.bashrc
 echo "alias ls=\"ls -l\"" >> ~/.bashrc
 echo "alias la=\"ls -la\"" >> ~/.bashrc
+
+echo "Setting up vim configuration..."
+
+cat << 'EOF' > ~/.vimrc
+set number
+syntax on
+set relativenumber
+set showmode
+
+" Move 1 more lines up or down in normal and visual selection modes.
+nnoremap E :m .-2<CR>==
+nnoremap N :m .+1<CR>==
+vnoremap E :m '<-2<CR>gv=gv
+vnoremap N :m '>+1<CR>gv=gv
+EOF
+
+
